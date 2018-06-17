fb103ce46f7a6624e4d98b4c118cb65621312969
jakehoare <jake_hoare@hotmail.com>
2018-06-17 18:27:22 +1000

    Update comments.

--- adaptable_priority_queue.py
+++ adaptable_priority_queue.py
@@ -7,44 +7,44 @@ class Apq_heapq():
     """
     Creates an adaptable priority queue from heapq.
     Dictionary is used to track entries in priority queue so items can be located in O(1).
-    Removed or updated entries remain in heapq with value of "<REMOVED>" and ignored by pop and peek.
+    Removed entries remain in heap with item of "<REMOVED>" and are ignored by pop and peek.
     Counter is used to ensure pop order is stable with respect to insert or update order.
     """
     def __init__(self):
         self.heap = []                      # underlying heap
-        self.value_mapping = {}             # map from value to entry of [priority, count, value]
+        self.item_mapping = {}              # map from item to list of [priority, count, item]
         self.counter = 0                    # count breaks priority ties so input/update order is preserved (stable)
 
-    def add_value(self, value, priority=0):
+    def add_item(self, item, priority = 0):
         """
-        Add or update value with specified priority.
-        :param value:
+        Add or update item with specified priority.
+        :param item:
         :param priority: float
         :return: None
         """
-        if value in self.value_mapping:     # remove old value from mapping and set to "REMOVED" in heap
-            self.remove_value(value)
-        entry = [priority, self.counter, value]
+        if item in self.item_mapping:       # remove old item from mapping and set to "<REMOVED>" in heap
+            self.remove_item(item)
+        entry = [priority, self.counter, item]
         self.counter += 1
-        self.value_mapping[value] = entry   # update mapping
+        self.item_mapping[item] = entry     # update mapping
         heapq.heappush(self.heap, entry)    # push entry to heap
 
-    def remove_value(self, value):
-        entry = self.value_mapping.pop(value)
+    def remove_item(self, item):
+        entry = self.item_mapping.pop(item)
         entry[-1] = "<REMOVED>"
 
     def pop_min(self):
         while self.heap:
-            _, _, value = heapq.heappop(self.heap)
-            if value != "<REMOVED>":        # throw away removed values
-                del self.value_mapping[value]
-                return value
+            _, _, item = heapq.heappop(self.heap)
+            if item != "<REMOVED>":        # throw away removed items
+                del self.item_mapping[item]
+                return item
         raise KeyError('pop from an empty priority queue')
 
     def peek_min(self):
         while self.heap:
-            _, _, value = self.heap[0]
-            if value != "<REMOVED>":
-                return value
-            heapq.heappop(self.heap)        # throw away removed values
+            _, _, item = self.heap[0]
+            if item != "<REMOVED>":
+                return item
+            heapq.heappop(self.heap)        # throw away removed items
         raise KeyError('peek at empty priority queue')
